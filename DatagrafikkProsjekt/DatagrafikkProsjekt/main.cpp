0a9229b8301bf043ba0cc30af812fee8f9d6233d
Mette <mette-strand@live.no>
2019-10-16 12:17:16 +0200

    Created x-code project

---
+++ DatagrafikkProsjekt/DatagrafikkProsjekt/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DatagrafikkProsjekt
+//
+//  Created by Mette Strand Hornnes on 16/10/2019.
+//  Copyright © 2019 Mette Strand Hornnes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

385b65de2e536c1adc6b73d3154f08ca113b1fba
Mette <mette-strand@live.no>
2019-11-22 19:51:31 +0100

    Working

--- DatagrafikkProsjekt/DatagrafikkProsjekt/main.cpp
+++ DatagrafikkProsjekt/DatagrafikkProsjekt/main.cpp
@@ -122,8 +122,9 @@ GLfloat cubeVertices[] =
      -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 0.0f, -1.0f, 0.0f,
      -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f, -1.0f, 0.0f
  };
- 
-         
+
+
+    
 // SKYBOX
 GLfloat skyboxVertices[] = {
     // Posisjoner

0b19467d47106da5a7a47842277bc85449216af5
Mette <mette-strand@live.no>
2019-11-27 22:14:51 +0100

    Testing shadows

--- DatagrafikkProsjekt/DatagrafikkProsjekt/main.cpp
+++ DatagrafikkProsjekt/DatagrafikkProsjekt/main.cpp
@@ -157,6 +157,7 @@ GLuint skyboxVAO, skyboxVBO;
 
 // Shadere
 Shader cubeShader;
+Shader depthQuad;
 Shader skyboxShader;
 
 // Textures
@@ -171,6 +172,8 @@ GLint cubeTextureLoc;
 GLint lightColorLoc;
 GLint lightPositionLoc;
 GLint viewPositionLoc;
+
+GLint depthMathLoc;
 
 // Skybox Uniform locations
 GLint projLocSkybox;
@@ -212,6 +215,44 @@ int initGL() {
 
    // Deaktiverer vertexarrayen
     glBindVertexArray(0);
+    
+    
+    
+    
+    
+    
+    // configure depth map FBO
+    // -----------------------
+    const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+    unsigned int depthMapFBO;
+    glGenFramebuffers(1, &depthMapFBO);
+    // create depth texture
+    unsigned int depthMap;
+    glGenTextures(1, &depthMap);
+    glBindTexture(GL_TEXTURE_2D, depthMap);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+    // attach depth texture as FBO's depth buffer
+    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+    glDrawBuffer(GL_NONE);
+    glReadBuffer(GL_NONE);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     
 // SKYBOX
@@ -235,6 +276,10 @@ int initGL() {
     
     // Setup and compile our shaders
     cubeShader = Shader( "resources/shaders/cube.vert", "resources/shaders/cube.frag" );
+    
+    depthQuad = Shader( "resources/shaders/depthQuad.vert", "resources/shaders/depthQuad.frag" );
+    
+    
     skyboxShader = Shader( "resources/shaders/skybox.vert", "resources/shaders/skybox.frag" );
 
     //Laste inn texture til kuben:
@@ -262,6 +307,10 @@ int initGL() {
     lightColorLoc = glGetUniformLocation(cubeShader.Program, "lightColor");
     lightPositionLoc = glGetUniformLocation(cubeShader.Program, "lightPos");
     viewPositionLoc = glGetUniformLocation(cubeShader.Program, "viewPos");
+    
+    
+    depthQuad.Use()
+    depthMathLoc = glGetUniformLocation(cubeShader.Program, "depthMap");
     
     
     // Henter inn uniform-loactions fra skybox-shader
@@ -334,6 +383,63 @@ void drawGLScene() {
     // Deaktiverer shaderprogram som brukes og vertexarray
     glUseProgram(0);
     glBindVertexArray(0);
+    
+    
+    depthQuad.Use()
+    glUniform1i(depthMathLoc , 0);
+    
+    
+    // 1. render depth of scene to texture (from light's perspective)
+    // --------------------------------------------------------------
+    glm::mat4 lightProjection, lightView;
+    glm::mat4 lightSpaceMatrix;
+    float near_plane = 1.0f, far_plane = 7.5f;
+    //lightProjection = glm::perspective(glm::radians(45.0f), (GLfloat)SHADOW_WIDTH / (GLfloat)SHADOW_HEIGHT, near_plane, far_plane); // note that if you use a perspective projection matrix you'll have to change the light position as the current light position isn't enough to reflect the whole scene
+    lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+    lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
+    lightSpaceMatrix = lightProjection * lightView;
+    // render scene from light's point of view
+    simpleDepthShader.use();
+    simpleDepthShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+
+    glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+        glClear(GL_DEPTH_BUFFER_BIT);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, woodTexture);
+        renderScene(simpleDepthShader);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    // reset viewport
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // 2. render scene as normal using the generated depth/shadow map
+    // --------------------------------------------------------------
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    shader.use();
+    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    glm::mat4 view = camera.GetViewMatrix();
+    shader.setMat4("projection", projection);
+    shader.setMat4("view", view);
+    // set light uniforms
+    shader.setVec3("viewPos", camera.Position);
+    shader.setVec3("lightPos", lightPos);
+    shader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, woodTexture);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, depthMap);
+    renderScene(shader);
+
+    // render Depth map to quad for visual debugging
+    // ---------------------------------------------
+    debugDepthQuad.use();
+    debugDepthQuad.setFloat("near_plane", near_plane);
+    debugDepthQuad.setFloat("far_plane", far_plane);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, depthMap);
     
     
 /* * * * * * *
